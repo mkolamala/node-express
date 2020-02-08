30c3ec92c2c97dd85a157ad6e81a85424e637f73
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-02-08 00:12:03 -0800

    updated campsite express router

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,23 +25,26 @@ campsiteRouter.route("/")
       res.end('Deleting all campsites');
   });
   
-//   app.get("/campsites/:campsiteId", (req, res) => {
-//       res.end(`Will send details of the campsite: ${req.params.campsiteId}`);
-//     });
-  
-//     app.post('/campsites/:campsiteId', (req, res) => {
-//       res.statusCode = 403;
-//       res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-//   });
-  
-//   app.put('/campsites/:campsiteId', (req, res) => {
-//       res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-//       res.end(`Will update the campsite: ${req.body.name}
-//           with description: ${req.body.description}`);
-//   });
-  
-//   app.delete('/campsites/:campsiteId', (req, res) => {
-//       res.end(`Deleting campsite: ${req.params.campsiteId}`);
-//   });
+  campsiteRouter.route("/:campsiteId")
+  .all( (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();// will pass the context to the corresponding http verb
+  })
+ .get((req, res) => {
+      res.end(`Will send details of the campsite: ${req.params.campsiteId}`);
+    })
+    .post((req, res) => {
+      res.statusCode = 403;
+      res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+  })
+  .put((req, res) => {
+      res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+      res.end(`Will update the campsite: ${req.body.name}
+          with description: ${req.body.description}`);
+  })
+  .delete((req, res) => {
+      res.end(`Deleting campsite: ${req.params.campsiteId}`);
+  });
 
 module.exports = campsiteRouter;
