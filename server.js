4c83243f36a6f56aeeddc004ea2be4d6bce02d98
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-02-07 23:52:28 -0800

    Express Simple REST

--- server.js
+++ server.js
@@ -1,20 +1,65 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.use((req,res) => {
-console.log(req.headers);
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+//any request to the path /campsites will trigger this method
+// all => all http verbs
+app.all("/campsites", (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();// will pass the context to the corresponding http verb
 });
 
-app.listen(port,hostname,()=> {
-    console.log(`Server runnign at http:\\${hostname}:${port}`);
-})
+app.get("/campsites", (req, res) => {
+  res.end("will send all the campsites to you");
+});
+
+app.post('/campsites',(req,res)=>{
+  res.end(`will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get("/campsites/:campsiteId", (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId}`);
+  });
+
+  app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server runnign at http:\\${hostname}:${port}`);
+});
